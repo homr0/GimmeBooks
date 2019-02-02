1e42acd078354187d4f27f88269a0fcc02c30cb8
Rebecca Hom <rebecca.hom001@gmail.com>
2019-02-02 10:37:07 -0800

    linted JS files

--- public/js/books.js
+++ public/js/books.js
@@ -1,29 +1,45 @@
-$(document).ready(function () {
-    $("#bookForm").submit(function () {
-        var search = $("#search_bar").val();
-        console.log(search);
-        if (search === "") {
-            alert("Please do not leave blank");
+$(document).ready(function() {
+  $("#bookForm").submit(function() {
+    var search = $("#search_bar").val();
+    console.log(search);
+    if (search === "") {
+      alert("Please do not leave blank");
+    } else {
+      $.get(
+        (query = "https://www.googleapis.com/books/v1/volumes?q=" + search),
+        function(response) {
+          console.log(response);
+
+          for (i = 0; i < response.items.length; i++) {
+            var book = response.items[i].volumeInfo;
+
+            var title = $("<h5>")
+              .addClass("center align black-text")
+              .text(book.title);
+
+            var author = $("<h5>")
+              .addClass("center align black-text")
+              .text(book.authors);
+
+            var img = $("<img>")
+              .addClass("aligning card z-depth-5")
+              .attr({
+                id: "dynamic" + i,
+                alt: book.title,
+                src: book.imageLinks.thumbnail
+              });
+
+            var button = $("<a>")
+              .addClass("btn blue imageButton")
+              .attr({
+                href: book.infoLink
+              });
+
+            $("#search_results").append(title, author, img, button);
+          }
         }
-        else {
-            var title = "";
-            var author = "";
-            var img = "";
-            var url = "";
-            $.get("https://www.googleapis.com/books/v1/volumes?q=" + search, function (response) {
-                console.log(response);
-                for (i = 0; i < response.items.length; i++) {
-                    title = $('<h5 class="center align black-text">' + response.items[i].volumeInfo.title + '</h5>');
-                    author = $('<h5="center align black-text">' + response.items[i].volumeInfo.authors + '</h5>');
-                    img = $('<img="aligning card z-depth-5 id="dynamic"> <br> <a href=' + response.items[i].volumeInfo.infoLink + '> <button id = "imageButton" class="btn blue">Read More</button></a>');
-                    url = response.items[i].volumeInfo.imageLinks.thumbnail;
-                    img.attr("src", url);
-                    title.appendTo("#search_results");
-                    author.appendTo("#search_results");
-                    img.appendTo("#search_results");
-                }
-            });
-        }
-        return false;
-    });
+      );
+    }
+    return false;
+  });
 });
