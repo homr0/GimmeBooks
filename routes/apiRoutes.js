7de794d79e618745c6bdf93dbada74824a09c118
Meenal <meenalalagappan89@gmail.com>
2019-02-03 00:36:12 -0800

    initating authorization for passport.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -58,6 +58,21 @@ module.exports = function(app) {
   });
 };
 
+//passport.js /login you will get redirected to auth0, to a page where you can select the identity provider
+app.get('/callback',
+passport.authenticate('auth0', {failureRedirect: '/login' }),
+function(req, res) {
+  if(!req.user) {
+    throw new Error ('user null');
+  }
+res.redirect("/")
+});
+
+app.get('/login',
+passport.authenticate('auth0', {}), function (req, res) {
+  res.redirect("/");
+});
+
 
       
   
