e9b20027b290d8c8505fdb2f5586ab2df0489d44
Meenal <meenalalagappan89@gmail.com>
2019-02-04 10:54:48 -0800

    pulling

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,9 +5,9 @@ module.exports = function(app) {
   app.post("/api/user", function(req, res) {
     // findAll returns all enteries for a table when used with no options
     db.User.create({
-      name:req.body.name,
-      email:req.body.email,
-      password:req.body.password
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
     }).then(function(dbUser) {
       res.json(dbUser);
     });
@@ -15,53 +15,47 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/favorites", function(req, res) {
-    db.favoriteBooks.create({
-      title:req.body.title,
-      author:req.body.author,
-      genre:req.body.genre,
-      year:req.body.year,
-      ISBN:req.body.ISBN,
-      id:req.body.id
-      }).then(function(dbUser) {
-      res.json(dbUser);
-    });
+    db.favoriteBooks
+      .create({
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        year: req.body.year,
+        ISBN: req.body.ISBN,
+        id: req.body.id
+      })
+      .then(function(dbUser) {
+        res.json(dbUser);
+      });
   });
 
   // Delete an example by id
   app.delete("/api/favorites/:id", function(req, res) {
-      db.User
-      .destroy({
-        where: { 
+    db.User.destroy({
+      where: {
         id: req.params.id
-       }
-     }).then(function(dbfavoriteBooks) {
+      }
+    }).then(function(dbfavoriteBooks) {
       res.json(dbfavoriteBooks);
     });
   });
 
-
   // PUT route for updating favoriteBooks. We can get the updated fav books data from req.body
   app.get("/user/:id", function(req, res) {
     db.favoriteBooks
-    .update(
-      {
-        title: req.body.title,
-        authors: req.body.authors
-      },
-      {
-        where: {
-          id: req.body.id
-      }
-    }).then(function(favoriteBooks) {
-      res.json(favoriteBooks);
-    });
+      .update(
+        {
+          title: req.body.title,
+          authors: req.body.authors
+        },
+        {
+          where: {
+            id: req.body.id
+          }
+        }
+      )
+      .then(function(favoriteBooks) {
+        res.json(favoriteBooks);
+      });
   });
 };
-
-
-
-
-
-      
-  
-
