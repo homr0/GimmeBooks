ec01ceb143618e8d0903ca4df71e55ad5b02ef1a
Meenal <meenalalagappan89@gmail.com>
2019-02-04 10:46:00 -0800

    pulling

--- models/user.js
+++ models/user.js
@@ -1,30 +1,24 @@
 module.exports = function(sequelize, Sequelize) {
-  var User = sequelize.define('User', {
-
+  var User = sequelize.define("User", {
     userName: {
       type: Sequelize.STRING,
-      notEmpty: true,
+      notEmpty: true
     },
 
     email: {
       type: Sequelize.STRING,
       primaryKey: true,
       validate: {
-        isEmail: true,
+        isEmail: true
       }
     },
-        
     password: {
       type: Sequelize.STRING,
-      allowNull: false,
+      allowNull: false
     }
   });
-
   User.associate = function(models) {
-
     User.hasMany(models.favoriteBooks);
-  }
-    
-
+  };
   return User;
-}
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -58,20 +58,8 @@ module.exports = function(app) {
   });
 };
 
-//passport.js /login you will get redirected to auth0, to a page where you can select the identity provider
-app.get('/callback',
-passport.authenticate('auth0', {failureRedirect: '/login' }),
-function(req, res) {
-  if(!req.user) {
-    throw new Error ('user null');
-  }
-res.redirect("/")
-});
 
-app.get('/login',
-passport.authenticate('auth0', {}), function (req, res) {
-  res.redirect("/");
-});
+
 
 
       
