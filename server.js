fec0c62490fd7a7ed961749a0085362d59326475
Rebecca Hom <rebecca.hom001@gmail.com>
2019-02-05 22:10:20 -0800

    removed body-parser references and set sequelize database to sync true

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser");
 var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static("public"));
-app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extendended: true
   })
 );
@@ -29,12 +26,15 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+var syncOptions = {
+  force: true
+};
+
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-var syncOptions;
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
