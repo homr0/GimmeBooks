17f1f641e09cfb2a605521330707f57d8ef6e971
Rebecca Hom <rebecca.hom001@gmail.com>
2019-02-05 19:08:55 -0800

    fixed up ids for buttons so that sign up works

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,36 @@
-$(document).ready(function () {
-  $('.collapsible').collapsible();
+$(document).ready(function() {
+  $(".collapsible").collapsible();
 
-  $('.dropdown-trigger').dropdown({
+  $(".dropdown-trigger").dropdown({
     closeOnClick: false,
     constrainWidth: false
   });
 
-  // AJAX 
+  // AJAX
   $("#signUpButton").on("click", function(e) {
     e.preventDefault();
 
     var newUser = {
-      name: $("#user_name").val().trim(),
-      email: $("#signEmail").val().trim(),
-      password: $("#signPassword").val().trim()
+      name: $("#user_name")
+        .val()
+        .trim(),
+      email: $("#signEmail")
+        .val()
+        .trim(),
+      password: $("#signPassword")
+        .val()
+        .trim()
     };
 
     console.log(newUser);
 
-    $.ajax("/api/users", {
+    $.ajax("/register", {
       method: "POST",
       data: newUser
     }).then(function() {
       console.log("Created New User");
       location.reload();
-    })
+    });
   });
 });
 
