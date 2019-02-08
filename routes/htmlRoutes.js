7b52b96f421ff52a22b5d0a645ff26a33ed96418
Meenal <meenalalagappan89@gmail.com>
2019-02-07 20:28:38 -0800

    authentication added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../configure/middleware/isAuthenticated");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   // Load index page
@@ -17,5 +17,11 @@ module.exports = function(app) {
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the login page
+  app.get("/login", isAuthenticated, function(req, res) {
+    res.sendfile(path.join(__dirname, "../views/user.handlebars"));
   });
 };
